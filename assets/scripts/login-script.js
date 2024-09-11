ed53f84855d437ecad122d29ffb570e63cb1599b
resti-kusumaasri <kusumaasriresti@gmail.com>
2024-09-11 13:44:47 +0700

    Update 1 login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,8 +5,24 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script */
+/**
+* membuat variabel lognFormElement untuk tampilan form.
+ @constat {HTMLElement}
+ */
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+* Membuat variabel inputEmailElement untuktampilan input email.
+* @constant {HTMLElement}
+*/
+
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+* Membuat variabek inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
 const expectedEmail = 'admin@dicoding.com';
